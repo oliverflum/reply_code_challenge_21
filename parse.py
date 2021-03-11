4f754b87b0df9746d8b40f34a81de986f0e37f1b
Oliver Flum <o.flum@reply.de>
2021-03-11 17:21:56 +0100

    Parsed

---
+++ parse.py
@@ -0,0 +1,56 @@
+import json
+
+
+def pars(file_name):
+  in_file = open("inputs/"+file_name + ".in", 'r')
+  lines = in_file.readlines()
+
+  dimensions = [int(lines[0].split(' ')[0]),int(lines[0].split(' ')[1])]
+  print(dimensions)
+
+  split_stats = lines[1].strip().split(' ')
+
+  no_buildings = int(split_stats[0])
+  no_antennas = int(split_stats[1])
+  reward = int(split_stats[2])
+
+  print(no_buildings, no_antennas, reward)
+
+  buildings_start_index = 2
+  buildings_end_index = buildings_start_index + no_buildings
+  print("BUILDINGS", buildings_start_index, buildings_end_index)
+  antennas_start_index = buildings_end_index
+  antennas_end_index = len(lines)
+
+  entities = {
+    "buildings": [],
+    "antennas": [],
+    "reward": reward
+  }
+
+  for i in range(buildings_start_index, buildings_end_index):
+    building_props = lines[i].strip().split(" ")
+    entities["buildings"].append({
+      "index": i-buildings_start_index,
+      "x": building_props[0],
+      "y": building_props[1],
+      "latency": building_props[2],
+      "speed": building_props[3]
+    })
+
+  for i in range(antennas_start_index, antennas_end_index):
+    antenna_props = lines[i].strip().split(" ")
+    entities["antennas"].append({
+      "index": i-antennas_start_index,
+      "range": antenna_props[0],
+      "speed": antenna_props[1]
+    })
+
+  if len(entities["buildings"]) != no_buildings or len(entities["antennas"]) != no_antennas:
+    100/0
+
+  with open("outputs/"+file_name + ".out", 'w') as fp:
+    json.dump(entities, fp)
+
+for file in ["data_scenarios_a_example", "data_scenarios_b_mumbai", "data_scenarios_c_metropolis", "data_scenarios_d_polynesia", "data_scenarios_e_sanfrancisco", "data_scenarios_f_tokyo"]:
+  pars(file)
