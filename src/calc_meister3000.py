d79cf8b14210c836f8f0396eaf99fe1683af4fce
Oliver Flum <o.flum@reply.de>
2021-03-11 20:28:42 +0100

    stuff

--- src/calc_meister3000.py
+++ src/calc_meister3000.py
@@ -1,7 +1,7 @@
 def calcScore(buildings, antennas, reward):
   building_antennas= {}
   for a in antenna:
-    buildings_in_range = list(filter(lambda x: x["x"] < , buildings))
+    buildings_in_range = list(filter(lambda x: x["x"] < x["x"]+a["range"] and , buildings))
 
 for file in ["data_scenarios_a_example", "data_scenarios_b_mumbai", "data_scenarios_c_metropolis", "data_scenarios_d_polynesia", "data_scenarios_e_sanfrancisco", "data_scenarios_f_tokyo"]:
   with open('placed/'+file+'.json') as json_file:
