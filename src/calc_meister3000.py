9aa1247a388c3dc4752abc7306a8c06a597c14c2
Oliver Flum <o.flum@reply.de>
2021-03-11 20:13:50 +0100

    stuff

---
+++ src/calc_meister3000.py
@@ -0,0 +1,7 @@
+for file in ["data_scenarios_a_example", "data_scenarios_b_mumbai", "data_scenarios_c_metropolis", "data_scenarios_d_polynesia", "data_scenarios_e_sanfrancisco", "data_scenarios_f_tokyo"]:
+  with open('placed/'+file+'.json') as json_file:
+    entities = json.load(json_file)
+  new_score = calcScore(entities["buildings"], entities["antennas"], entities["reward"])
+  if new_score > entities['score']:
+      print("publish me")
+      print(new_score)
