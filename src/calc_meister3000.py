84e3d3da76f8b7311d70cbcbda9fd9f25c7f452f
Oliver Flum <o.flum@reply.de>
2021-03-13 14:18:13 +0100

    Forgot reward

--- src/calc_meister3000.py
+++ src/calc_meister3000.py
@@ -3,8 +3,8 @@ import numpy
 import random
 
 total = 0
-#for file in ["data_scenarios_a_example", "data_scenarios_b_mumbai", "data_scenarios_c_metropolis", "data_scenarios_d_polynesia", "data_scenarios_e_sanfrancisco", "data_scenarios_f_tokyo"]:
-for file in ["data_scenarios_a_example"]:
+for file in ["data_scenarios_a_example", "data_scenarios_b_mumbai", "data_scenarios_c_metropolis", "data_scenarios_d_polynesia", "data_scenarios_e_sanfrancisco", "data_scenarios_f_tokyo"]:
+#for file in ["data_scenarios_a_example"]:
   with open('../placed/'+file+'.json') as json_file:
     entities = json.load(json_file)
   grid_width = entities["grid_width"]
